c0896ba1ebc4314b39f39b81388a565afd3fbd3e
Santosh Yadav <santosh@coderabbit.ai>
2026-02-26 17:04:14 +0100

    chore: add template syntax and signals

--- apps/hotelmanagement/src/app/dashboard/dashboard.ts
+++ apps/hotelmanagement/src/app/dashboard/dashboard.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { single } from 'rxjs';
 
 interface Hotel {
+  id: number;
   name: string;
   location: string;
+  numberOfRooms: number;
 }
 
 @Component({
@@ -12,15 +15,32 @@ interface Hotel {
   styleUrl: './dashboard.css',
 })
 export class Dashboard {
+  title = signal('Welcome to the Hotel Management Dashboard!');
 
-  title = 'Welcome to the Hotel Management Dashboard!';
-
-  numberOfBookings = 0;
+  numberOfBookings = signal(10);
 
   dateOfLastBooking: Date | null = null;
 
-  hotelList : Hotel[] = [
-  ];
+  hotelList = signal<Hotel[]>([
+    {
+      id: 1,
+      name: 'Hotel California',
+      location: 'Los Angeles',
+      numberOfRooms: 0,
+    },
+    {
+      id: 2,
+      name: 'The Grand Budapest Hotel',
+      location: 'Zubrowka',
+      numberOfRooms: 200,
+    },
+    {
+      id: 3,
+      name: 'The Overlook Hotel',
+      location: 'Colorado',
+      numberOfRooms: 10,
+    },
+  ]);
 
-
+  propertyTypes = signal<'Hotel' | 'Motel' | 'Resort' | 'Hostel'>('Hotel');
 }
