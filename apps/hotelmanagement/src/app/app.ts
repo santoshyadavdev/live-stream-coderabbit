6dfe59c0f86e7c2fc06d5ff64b9c1c3be02d14d8
Santosh Yadav <santosh@coderabbit.ai>
2026-03-11 17:42:05 +0100

    add api integration

--- apps/hotelmanagement/src/app/app.ts
+++ apps/hotelmanagement/src/app/app.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { Dashboard } from './dashboard/dashboard';
 import { PropertyService } from './dashboard/property';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   imports: [RouterModule, Dashboard],
@@ -13,4 +14,10 @@ export class App {
   protected title = 'hotelmanagement';
 
   hotelList = inject(PropertyService).getHotels();
+
+  hotels = inject(HttpClient)
+    .get('/api/hotels')
+    .subscribe((data) => {
+      console.log(data);
+    });
 }
